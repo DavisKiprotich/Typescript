0ed110aea7642434c86d7937ab5e6f63cc9dc2f0
Davis Kiprotich <100116526+DavisKiprotich@users.noreply.github.com>
2023-08-05 11:48:08 +0300

    Create myObjects.ts

---
+++ myObjects.ts
@@ -0,0 +1,15 @@
+// Declaring Objects
+const user = {
+    name: "Karoti",
+    isPaid: true
+}
+
+function createUser  ({name: string, isFined: boolean}){}
+createUser({name: "Kaka", isFined: false})
+
+function createAnswer():{}{   //First curly describes if param are string, number e.t.c
+    return {}
+}
+
+
+export{}
