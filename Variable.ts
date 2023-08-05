8baa8c3b0a7489dbe117f03849f38b6fa1cea938
Davis Kiprotich <100116526+DavisKiprotich@users.noreply.github.com>
2023-08-05 10:58:53 +0300

    Update Variable.ts

--- Variable.ts
+++ Variable.ts
@@ -1,3 +1,26 @@
 let greeting: string = "Chamgei"
 
+greeting.toUpperCase()
 console.log(greeting)  
+
+// any
+ 
+ let boy: string
+
+function identifyBoy(){
+    return "Karoti"
+}
+boy = identifyBoy()
+
+// Declaring functions
+
+function addNum(num: number){
+    return num + 3
+}
+addNum(5)
+
+function toLower(val: string){
+    return val.toLowerCase()
+}
+toLower("MAMBET")
+console.log(toLower("HI"))
