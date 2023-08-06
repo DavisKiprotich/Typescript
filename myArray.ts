8f44b488d68b3eba103b7c9233eca7bffed6b7a0
Davis Kiprotich <100116526+DavisKiprotich@users.noreply.github.com>
2023-08-06 21:50:55 +0300

    Create myArray.ts
    Worked on how to declare arrays while assigning their types and inheriting their values

---
+++ myArray.ts
@@ -0,0 +1,16 @@
+// Declaring arrays
+const lineup: string[] = []
+const number: Array<number> = []
+
+type User = {
+  name: string,
+  isFun: boolean
+}
+const allUsers : User[] = []
+const NLModels: number[][] = [
+  [244,233,222]
+]
+
+lineup.push("Lukaku")
+number.push(2)
+allUsers.push({name: "", isFun: true})
