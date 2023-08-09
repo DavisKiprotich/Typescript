6b5af5870e19c3ef4fa9494c3a48af7974b6e3e3
Davis Kiprotich <100116526+DavisKiprotich@users.noreply.github.com>
2023-08-09 10:19:31 +0300

    Update index.ts
    using get and set whereby set has no return type while get helps one get a property and it has a return type

--- index.ts
+++ index.ts
@@ -7,5 +7,21 @@ class User12{
         this.email: email,
         this.id: id
     }
+// getters
+get getAppEmail(): string{
+    return `apply ${this.email}`
+}
+get getCourse(): number{
+    return this.id
+}
+
+// setters does not have a return type
+
+set getCourse(Nume){
+    if(Nume <= 1){
+        throw new Error
+    }
+    this.getCourse = Nume
+}
 }
 let hsfsg= new User12("vahvhv@g.com", 45566)
