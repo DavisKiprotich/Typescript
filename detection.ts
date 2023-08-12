63211b0032c7151f05802440753830a5e6bc1a3f
Davis Kiprotich <100116526+DavisKiprotich@users.noreply.github.com>
2023-08-12 09:23:44 +0300

    Create detection.ts

---
+++ detection.ts
@@ -0,0 +1,28 @@
+function detectTypes(val: number | string){
+  if(typeof val === "string"){
+    return val.toLowerCase()
+  }
+  return val + 3
+}
+
+function provideID(id: string | null){
+  if(!id){
+    console.log("Please provide id")
+    return
+  }
+  id.toLowerCase()
+}
+
+// Type Narrowing
+function printAll(strs: string | string[] | null) {
+
+  if (strs) {
+    if (typeof strs === "object") {
+      for (const s of strs) {
+        console.log(s);
+      }
+    } else if (typeof strs === "string") {
+      console.log(strs);
+    }
+  }
+}
