99ae15a22786119dfe7cab2771a9cf0e10319b8d
Davis Kiprotich <100116526+DavisKiprotich@users.noreply.github.com>
2023-08-12 09:43:03 +0300

    Update detection.ts
    Done with narrowing, typeof instances and in narrowing

--- detection.ts
+++ detection.ts
@@ -26,3 +26,35 @@ function printAll(strs: string | string[] | null) {
     }
   }
 }
+// in operator narrowing
+interface User{
+    name: string,
+    id: number
+}
+interface Admin{
+    name: string,
+    id: number,
+    isAdmin: boolean
+}
+function isAdminAcc(account: User | Admin){
+    if("isAdmin" in account){
+        return account.isAdmin
+    }
+}
+
+// type predicate
+type Fish= {swim: () => void}
+type Bird = {fly: () => void}
+function isFish(pet: Fish | Bird): pet is Fish{
+    return (pet as Fish).swim !== undefined
+}
+
+function getFood (pet: Fish | Bird){
+    if(isFish(pet)){
+        pet
+        return "Fish food"
+    }
+    pet
+    return "Bird Food"
+}
+
